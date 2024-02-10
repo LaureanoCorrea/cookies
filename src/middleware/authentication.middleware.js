d2f26872434568aefb9f7c24d4b3832c92b1ec49
laureano correa <loriensdesign@gmail.com>
2024-02-10 19:51:52 -0300

    Delete src directory

--- src/middleware/authentication.middleware.js
+++
@@ -1,8 +0,0 @@
-function auth (req, res, next) {
-    if (req.session?.user.username === 'fede' && req.session.user.admin) {
-        return next ()
-    }
-    return res.status(401).send('Error de Autenticacion')
-}
-
-export default auth
